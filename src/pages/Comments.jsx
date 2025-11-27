e54a9d4ccfbadbd69efe7d766103de22bdfcb41a
leondev <leon.dev@gmx.net>
2025-11-27 16:41:29 +0100

    Comments - images

--- src/pages/Comments.jsx
+++ src/pages/Comments.jsx
@@ -1,16 +1,19 @@
 import Jungle from "../assets/img/galary/wedding/jungle.webp";
+import Concert from "../assets/img/hero/event/concert.webp";
 
 function Comments() {
   return (
     <section className="flex max-md:flex-col items-center justify-center">
       <Comment
         img={Jungle}
+        height="16.0625rem"
         heading="Wunderschöne Hochzeitsfotos!"
         text="Lukas hat unsere Hochzeit perfekt eingefangen. Die Fotos sind emotional,
         natürlich und wunderschön!"
       />
       <Comment
-        img={Jungle}
+        img={Concert}
+        height="14.3125rem"
         heading="Energie des Konzerts großartig eingefangen."
         text="Atmosphärische Aufnahmen, die Künstler und Publikum begeistert haben."
       />
@@ -22,7 +25,9 @@ function Comment(props) {
   return (
     <div className="flex gap-5 flex-col items-center justify-center w-108 p-10 h-auto text-center">
       <img
-        className="h-64.25 rounded-[1.5625rem] shadow-full"
+        // className="h-64.25 rounded-[1.5625rem] shadow-full"
+        style={{ height: props.height }}
+        className="rounded-[1.5625rem] shadow-full"
         src={props.img}
         alt=""
       />
